79197fc8533f6dc3e941bd99397479d84f7f0588
cezar-r <59450965+cezar-r@users.noreply.github.com>
2021-09-16 23:13:57 -0700

    Update error_messages.py

--- src/error_messages.py
+++ src/error_messages.py
@@ -6,6 +6,7 @@ This file is used to throw custom errors
 '''
 
 class ArgumentError(Exception):
+    """This error is raised when invalid arguments are passed in"""
     def __init__(self, message):            
         # Call the base class constructor with the parameters it needs
         super().__init__(message)
